357ad07c56ce4667429946418671ad82dbe4c042
Mohamed Tolba <tolbaax@gmail.com>
2023-06-17 01:14:40 +0300

    caching userdata

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -1,6 +1,5 @@
 class EndPoints {
-  static const String baseUrl =
-      'http://momedsalah-001-site1.atempurl.com/api';
+  static const String baseUrl = '';
   static const String register = '$baseUrl/User/Register';
   static const String login = '$baseUrl/User/Login';
 }
